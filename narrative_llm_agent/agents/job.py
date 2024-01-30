d39b7295fc8b34ce240be81ed3431391424fd731
Bill Riehl <wjriehl@lbl.gov>
2024-01-30 13:04:11 -0500

    change job agent role

--- narrative_llm_agent/agents/job.py
+++ narrative_llm_agent/agents/job.py
@@ -4,7 +4,7 @@ from narrative_llm_agent.tools.job_tools import job_status
 
 def job_agent(llm) -> Agent:
     return Agent(
-        role="Bioinformaticist and Data Scientist",
+        role="Job Manager",
         goal="Manage app and job running and tracking in the KBase system. Start and monitor jobs using the KBase Execution engine.",
         backstory="""You are an expert computer engineer. You are responsible for initializing, running, and monitoring
         KBase applications using the Execution Engine. You work with the rest of your crew to run bioinformatics and
