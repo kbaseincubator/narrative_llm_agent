c2aaa25d3a9269fb98b15fd0ade2b52a6dbc3696
Bill Riehl <wjriehl@lbl.gov>
2024-03-13 12:51:46 -0400

    add configurable database path vars

--- narrative_llm_agent/agents/analyst.py
+++ narrative_llm_agent/agents/analyst.py
@@ -8,23 +8,65 @@ from langchain.memory import ConversationBufferMemory, ReadOnlySharedMemory
 from langchain.chains import RetrievalQA
 from langchain.tools import tool
 import os
+from pathlib import Path
 
 
 class AnalystInput(BaseModel):
-    input: str = Field(description="query to look up KBase documentation, catalog or tutorials")
+    input: str = Field(
+        description="query to look up KBase documentation, catalog or tutorials"
+    )
+
 
 class AnalystAgent(KBaseAgent):
-    role="Computational Biologist and Geneticist"
-    goal="Analyze and interpret datasets, and make suggestions into next analysis steps."
-    backstory="""You are an expert academic computational biologist with decades of
+    role = "Computational Biologist and Geneticist"
+    goal = (
+        "Analyze and interpret datasets, and make suggestions into next analysis steps."
+    )
+    backstory = """You are an expert academic computational biologist with decades of
     experience working in microbial genetics. You have published several genome announcement
     papers and have worked extensively with novel sequence data."""
     _openai_key: str
+    _catalog_db_dir: Path = Path(__file__).parent / "vector_db_app_catalog"
+    _docs_db_dir: Path = Path(__file__).parent / "vector_db_kbase_docs"
 
-    def __init__(self: "AnalystAgent", token: str, llm: LLM, openai_api_key=None):
+    def __init__(
+        self: "AnalystAgent",
+        token: str,
+        llm: LLM,
+        openai_api_key: str = None,
+        catalog_db_dir: Path | str = None,
+        docs_db_dir: Path | str = None,
+    ):
         super().__init__(token, llm)
         self.__setup_openai_api_key(openai_api_key)
+        if catalog_db_dir is not None:
+            self._catalog_db_dir = Path(catalog_db_dir)
+        if docs_db_dir is not None:
+            self._docs_db_dir = Path(docs_db_dir)
+        for db_path in [self._catalog_db_dir, self._docs_db_dir]:
+            self.__check_db_directories(db_path)
         self.__init_agent()
+
+    def __check_db_directories(self, db_path: Path) -> None:
+        """
+        Checks for presence of the expected database directory. Doesn't look for all files,
+        just ensures that the directory is present and it has a `chroma.sqlite3` file.
+        If file or directory is missing, this raises a RuntimeError.
+        TODO: check for other files if needed
+        """
+        if not db_path.exists():
+            raise RuntimeError(
+                f"database directory {db_path} not found, unable to make Agent."
+            )
+        if not db_path.is_dir():
+            raise RuntimeError(
+                f"database directory {db_path} is not a directory, unable to make Agent."
+            )
+        db_file = db_path / "chroma.sqlite3"
+        if not db_file.exists():
+            raise RuntimeError(
+                f"Database file {db_file} not found, unable to make Agent."
+            )
 
     def __setup_openai_api_key(self, openai_api_key: str) -> None:
         if openai_api_key is not None:
@@ -35,19 +77,30 @@ class AnalystAgent(KBaseAgent):
             raise KeyError("Missing environment variable OPENAI_API_KEY")
 
     def __init_agent(self: "AnalystAgent") -> None:
-
-        @tool("Kbase documentation retrieval tool", args_schema = AnalystInput, return_direct=True)
+        @tool(
+            "Kbase documentation retrieval tool",
+            args_schema=AnalystInput,
+            return_direct=True,
+        )
         def kbase_docs_retrieval_tool(input: str):
-            """This tool has the KBase documentation. Useful for when you need to answer questions about how to use Kbase applications. Input should be a fully formed question. """
+            """This tool has the KBase documentation. Useful for when you need to answer questions about how to use Kbase applications. Input should be a fully formed question."""
             persist_directory = "./vector_db_kbase_docs"
-            return self._create_doc_chain(persist_directory=persist_directory).invoke({"query": input})
+            return self._create_doc_chain(persist_directory=persist_directory).invoke(
+                {"query": input}
+            )
 
-        @tool("Kbase app catalog retrieval tool", args_schema = AnalystInput, return_direct=True)
+        @tool(
+            "Kbase app catalog retrieval tool",
+            args_schema=AnalystInput,
+            return_direct=True,
+        )
         def kbase_appCatalog_retrieval_tool(input: str):
             """This tool has the KBase app catalog. Useful for when you need to find apps available in KBase.
-            All apps in the catalog also have name, version tooltip, categories and description to help you to decide which app to use. Input should be a fully formed question. """
+            All apps in the catalog also have name, version tooltip, categories and description to help you to decide which app to use. Input should be a fully formed question."""
             persist_directory = "./vector_db_app_catalog"
-            return self._create_doc_chain(persist_directory=persist_directory).invoke({"query": input})
+            return self._create_doc_chain(persist_directory=persist_directory).invoke(
+                {"query": input}
+            )
 
         self.agent = Agent(
             role=self.role,
@@ -56,22 +109,28 @@ class AnalystAgent(KBaseAgent):
             verbose=True,
             allow_delegation=True,
             llm=self._llm,
-            tools=[kbase_appCatalog_retrieval_tool, kbase_docs_retrieval_tool]
+            tools=[kbase_appCatalog_retrieval_tool, kbase_docs_retrieval_tool],
         )
 
     def _create_doc_chain(self, persist_directory):
-            # Embedding functions to use
-            embeddings = OpenAIEmbeddings(openai_api_key=self._openai_key)
-            # Use the persisted database
-            vectordb = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
-            retriever = vectordb.as_retriever()
+        # Embedding functions to use
+        embeddings = OpenAIEmbeddings(openai_api_key=self._openai_key)
+        # Use the persisted database
+        vectordb = Chroma(
+            persist_directory=persist_directory, embedding_function=embeddings
+        )
+        retriever = vectordb.as_retriever()
 
+        memory = ConversationBufferMemory(memory_key="chat_history")
+        readonlymemory = ReadOnlySharedMemory(memory=memory)
+        chain_type = "refine"
 
-            memory = ConversationBufferMemory(memory_key="chat_history")
-            readonlymemory = ReadOnlySharedMemory(memory=memory)
-            chain_type = 'refine'
+        # Retrieval chain
+        qa_chain = RetrievalQA.from_chain_type(
+            llm=self._llm,
+            chain_type=chain_type,
+            retriever=retriever,
+            memory=readonlymemory,
+        )
 
-            # Retrieval chain
-            qa_chain = RetrievalQA.from_chain_type(llm=self._llm, chain_type=chain_type, retriever=retriever,memory=readonlymemory)
-
-            return qa_chain
+        return qa_chain
--- tests/agents/test_analyst_agent.py
+++ tests/agents/test_analyst_agent.py
@@ -7,17 +7,20 @@ FAKE_OPENAI_KEY = "fake_openai_api_key"
 FAKE_OPENAI_KEY_ENVVAR = "not_an_openai_key_environment"
 OPENAI_KEY = "OPENAI_API_KEY"
 
-def test_init(mock_llm):
+
+def test_init_ok(mock_llm):
     wa = AnalystAgent(token, mock_llm, openai_api_key=FAKE_OPENAI_KEY)
     assert wa.role == "Computational Biologist and Geneticist"
     assert wa._token == token
     assert wa._openai_key == FAKE_OPENAI_KEY
+
 
 def test_init_with_env_var(mock_llm, monkeypatch):
     monkeypatch.setenv(OPENAI_KEY, FAKE_OPENAI_KEY_ENVVAR)
     wa = AnalystAgent(token, mock_llm)
     assert wa.role == "Computational Biologist and Geneticist"
     assert wa._token == token
+
 
 def test_init_fail_without_envvar(mock_llm, monkeypatch):
     if OPENAI_KEY in os.environ:
